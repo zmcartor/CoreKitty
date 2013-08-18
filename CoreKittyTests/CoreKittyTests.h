95e42eab3526ddf8698ba06ff3925f0322c7d21a
Zach <zmcartor@gmail.com>
2013-08-18 16:14:16 -0400

    Added test target

---
+++ CoreKittyTests/CoreKittyTests.h
@@ -0,0 +1,13 @@
+//
+//  CoreKittyTests.h
+//  CoreKittyTests
+//
+//  Created by zm on 8/18/13.
+//  Copyright (c) 2013 Hackazach. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CoreKittyTests : SenTestCase
+
+@end
---
+++ CoreKittyTests/CoreKittyTests.m
@@ -0,0 +1,32 @@
+//
+//  CoreKittyTests.m
+//  CoreKittyTests
+//
+//  Created by zm on 8/18/13.
+//  Copyright (c) 2013 Hackazach. All rights reserved.
+//
+
+#import "CoreKittyTests.h"
+
+@implementation CoreKittyTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CoreKittyTests");
+}
+
+@end
