c11551a8d98c50cb1d72050215d4abc6eae97e27
Zach <zmcartor@gmail.com>
2013-08-18 18:34:41 -0400

    V1 functionality test stubs

--- CoreKittyTests/CoreKittyTests.m
+++ CoreKittyTests/CoreKittyTests.m
@@ -10,10 +10,79 @@
 
 SPEC_BEGIN(COOLSPEC)
 
-describe(@"this is a cool spec", ^{
-    it(@"does a really great job", ^{
-        NSString *Zach = @"hello there!";
-        [[Zach should] equal:@"hello there!"];
+describe(@"CoreKitty", ^{
+    
+    beforeAll(^{
+        // Load up the fake Simpsons JSON
+    });
+    
+    it(@"counts the number of records", ^{
+            
+    });
+    
+    it (@"throws an exception if field name not found in Core Data model", ^{
+        
+    });
+    
+    
+    context(@"finders for given field and condition", ^{
+    
+        it(@"finds all records matching a string type field", ^{
+            
+        });
+        
+        it(@"finds all records matching an integer type field", ^{
+            
+        });
+        
+        it(@"finds all records matching a Boolean type field", ^{
+            
+        });
+
+        it(@"finds all records matching a date type field", ^{
+            
+        });
+
+        it(@"finds all records matching a field with integer value", ^{
+            
+        });
+        
+    });
+    
+    context(@"count functions for given field and condition", ^{
+        
+        it(@"counts records matching a string type field", ^{
+            
+        });
+        
+        it(@"counts records matching an integer type field", ^{
+            
+        });
+        
+        it(@"counts records matching a Boolean type field", ^{
+            
+        });
+        
+        it(@"counts records matching a date type field", ^{
+            
+        });
+        
+        it(@"counts records matching a field with integer value", ^{
+            
+        });
+        
+    });
+    
+    context (@"provides aggregate functions", ^{
+        it(@"computes the min of a given field", ^{
+        });
+        
+        it(@"computes the max of a given field", ^{
+        });
+        
+        it(@"computes the average of a given field", ^{
+        });
+        
     });
 });
 
