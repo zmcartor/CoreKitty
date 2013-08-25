53fe7a942954579213e02166d8339f3f134612fe
Zach <zmcartor@gmail.com>
2013-08-25 00:52:34 -0400

    Fix messed up otool configuration. Add CoreData store class

---
+++ CoreKittyTests/HKZStore.m
@@ -0,0 +1,13 @@
+//
+//  HKZStore.m
+//  CoreKitty
+//
+//  Created by zm on 8/24/13.
+//  Copyright (c) 2013 Hackazach. All rights reserved.
+//
+
+#import "HKZStore.h"
+
+@implementation HKZStore
+
+@end
