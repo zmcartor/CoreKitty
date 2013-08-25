22dff5024bf9594053386050fd1998f2b24dac4b
Zach <zmcartor@gmail.com>
2013-08-25 00:54:16 -0400

    HKZStore class

--- CoreKittyTests/HKZStore.h
+++ CoreKittyTests/HKZStore.h
@@ -10,4 +10,8 @@
 
 @interface HKZStore : NSObject
 
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
 @end
